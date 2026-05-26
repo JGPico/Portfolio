7463f391aa1442322215719b8bc2ed8322f06bf3
JGPico <minq_2@hotmail.com>
2026-05-26 12:11:48 -0600

    css flex exp

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ function App() {
 
   return (
     <>
-      <div className="row">
+      <div className="siteWrapper">
         <Header></Header>
-        <div className="column">
+        <div className="contentWrapper">
           <ContentBlock>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, et blanditiis cumque corporis fugit excepturi sed placeat at ipsum tenetur laborum dolore, suscipit quibusdam. Cumque accusamus quae impedit ab id.</ContentBlock>
           <ContentSub date="1/11/11" title="Banana">Potatoes are among the greatest of inventions</ContentSub>
         </div>
